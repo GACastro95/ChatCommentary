7b687cf9373c5777ed4eea2588fb0cb09318a95a
nLspriter <52107943+GACastro95@users.noreply.github.com>
2021-10-14 14:18:52 +0100

    Initial commit

---
+++ ChatCommentary/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace ChatCommentaryApp
+{
+    partial class ChatCommentaryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directoryListBox = new System.Windows.Forms.ListBox();
+            this.addDirectoryButton = new System.Windows.Forms.Button();
+            this.removeDirectoryButton = new System.Windows.Forms.Button();
+            this.enableRewardCheckbox = new System.Windows.Forms.CheckBox();
+            this.costLabel = new System.Windows.Forms.Label();
+            this.costBox = new System.Windows.Forms.TextBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.updateRewardButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // directoryListBox
+            // 
+            this.directoryListBox.FormattingEnabled = true;
+            this.directoryListBox.Location = new System.Drawing.Point(13, 13);
+            this.directoryListBox.Name = "directoryListBox";
+            this.directoryListBox.Size = new System.Drawing.Size(428, 95);
+            this.directoryListBox.TabIndex = 0;
+            // 
+            // addDirectoryButton
+            // 
+            this.addDirectoryButton.Location = new System.Drawing.Point(13, 114);
+            this.addDirectoryButton.Name = "addDirectoryButton";
+            this.addDirectoryButton.Size = new System.Drawing.Size(75, 23);
+            this.addDirectoryButton.TabIndex = 1;
+            this.addDirectoryButton.Text = "Add Directory";
+            this.addDirectoryButton.UseVisualStyleBackColor = true;
+            this.addDirectoryButton.Click += new System.EventHandler(this.AddDirectoryButton_Clicked);
+            // 
+            // removeDirectoryButton
+            // 
+            this.removeDirectoryButton.Location = new System.Drawing.Point(94, 114);
+            this.removeDirectoryButton.Name = "removeDirectoryButton";
+            this.removeDirectoryButton.Size = new System.Drawing.Size(75, 23);
+            this.removeDirectoryButton.TabIndex = 2;
+            this.removeDirectoryButton.Text = "Remove Directory";
+            this.removeDirectoryButton.UseVisualStyleBackColor = true;
+            this.removeDirectoryButton.Click += new System.EventHandler(this.RemoveDirectoryButton_Clicked);
+            // 
+            // enableRewardCheckbox
+            // 
+            this.enableRewardCheckbox.AutoSize = true;
+            this.enableRewardCheckbox.Location = new System.Drawing.Point(376, 114);
+            this.enableRewardCheckbox.Name = "enableRewardCheckbox";
+            this.enableRewardCheckbox.Size = new System.Drawing.Size(65, 17);
+            this.enableRewardCheckbox.TabIndex = 3;
+            this.enableRewardCheckbox.Text = "Enabled";
+            this.enableRewardCheckbox.UseVisualStyleBackColor = true;
+            this.enableRewardCheckbox.CheckedChanged += new System.EventHandler(this.EnabledCheckbox_Changed);
+            // 
+            // costLabel
+            // 
+            this.costLabel.AutoSize = true;
+            this.costLabel.Location = new System.Drawing.Point(12, 181);
+            this.costLabel.Name = "costLabel";
+            this.costLabel.Size = new System.Drawing.Size(28, 13);
+            this.costLabel.TabIndex = 4;
+            this.costLabel.Text = "Cost";
+            // 
+            // costBox
+            // 
+            this.costBox.Location = new System.Drawing.Point(47, 178);
+            this.costBox.Name = "costBox";
+            this.costBox.Size = new System.Drawing.Size(394, 20);
+            this.costBox.TabIndex = 5;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(12, 155);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(27, 13);
+            this.titleLabel.TabIndex = 4;
+            this.titleLabel.Text = "Title";
+            // 
+            // titleBox
+            // 
+            this.titleBox.Location = new System.Drawing.Point(47, 152);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(394, 20);
+            this.titleBox.TabIndex = 5;
+            // 
+            // updateRewardButton
+            // 
+            this.updateRewardButton.Location = new System.Drawing.Point(366, 204);
+            this.updateRewardButton.Name = "updateRewardButton";
+            this.updateRewardButton.Size = new System.Drawing.Size(75, 23);
+            this.updateRewardButton.TabIndex = 6;
+            this.updateRewardButton.Text = "Update";
+            this.updateRewardButton.UseVisualStyleBackColor = true;
+            this.updateRewardButton.Click += new System.EventHandler(this.UpdateButton_Clicked);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(12, 206);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Clicked);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Location = new System.Drawing.Point(175, 114);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(21, 23);
+            this.btnSettings.TabIndex = 14;
+            this.btnSettings.Text = "⚙️";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // ChatCommentaryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 241);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.updateRewardButton);
+            this.Controls.Add(this.titleBox);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.costBox);
+            this.Controls.Add(this.costLabel);
+            this.Controls.Add(this.enableRewardCheckbox);
+            this.Controls.Add(this.removeDirectoryButton);
+            this.Controls.Add(this.addDirectoryButton);
+            this.Controls.Add(this.directoryListBox);
+            this.Name = "ChatCommentaryForm";
+            this.Text = "Chat Commentary";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox directoryListBox;
+        private System.Windows.Forms.Button addDirectoryButton;
+        private System.Windows.Forms.Button removeDirectoryButton;
+        private System.Windows.Forms.CheckBox enableRewardCheckbox;
+        private System.Windows.Forms.Label costLabel;
+        private System.Windows.Forms.TextBox costBox;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.Button updateRewardButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button btnSettings;
+    }
+}
+
---
+++ ChatCommentary/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Windows.Forms;
+using TwitchLib.PubSub;
+using TwitchLib.PubSub.Events;
+using TwitchLib.Api;
+using System.IO;
+using System.Media;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChatCommentaryApp
+{
+    public partial class ChatCommentaryForm : Form
+    {
+        private static TwitchPubSub client;
+        private static TwitchAPI api;
+        private static SoundPlayer soundPlayer;
+        private static List<string> directories = new List<string>();
+
+        public ChatCommentaryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new TwitchPubSub();
+
+            client.OnPubSubServiceConnected += OnPubSubServiceConnected;
+            client.OnListenResponse += OnListenResponse;
+            client.OnChannelPointsRewardRedeemed += OnChannelPointsRewardRedeemed;
+            client.ListenToChannelPoints(Properties.Settings.Default.ChannelID);
+            client.Connect();
+
+            api = new TwitchAPI();
+
+            api.Settings.ClientId = Properties.Settings.Default.ClientID;
+            api.Settings.AccessToken = Properties.Settings.Default.AccessToken;
+
+            if (File.Exists("settings.txt"))
+            {
+                String[] loadSettings = File.ReadAllLines("settings.txt");
+                foreach (string line in loadSettings)
+                {
+                    directories.Add(line);
+                }
+            }
+
+            CreateReward();
+
+            ReloadListItems();
+        }
+
+        private static void OnPubSubServiceConnected(object sender, EventArgs e)
+        {
+            client.SendTopics(Properties.Settings.Default.AccessToken);
+        }
+
+        private static void OnListenResponse(object sender, OnListenResponseArgs e)
+        {
+            if (!e.Successful)
+                throw new Exception($"Failed to listen! Response: {e.Response}");
+        }
+
+        private void OnChannelPointsRewardRedeemed(object sender, OnChannelPointsRewardRedeemedArgs e)
+        {
+            if (e.RewardRedeemed.Redemption.Reward.Id == Properties.Settings.Default.RewardID && e.RewardRedeemed.Redemption.Status != "ACTION_TAKEN")
+            {
+                var rand = new Random();
+                List<string> files = new List<string>();
+                foreach (string directory in directories)
+                {
+                    files.AddRange(Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".mp3") || s.EndsWith(".wav")));
+                }
+                PlaySound(files[rand.Next(files.Count)]);
+            }
+        }
+
+        public void PlaySound(string filename, EventHandler doneCallback = null)
+        {
+
+            using (soundPlayer = new SoundPlayer())
+            {
+                soundPlayer.SoundLocation = filename;
+                soundPlayer.Load();
+                soundPlayer.PlaySync();
+            }
+            doneCallback?.Invoke(this, new EventArgs());
+        }
+
+        private void ReloadListItems()
+        {
+            directoryListBox.Items.Clear();
+
+            foreach (string directory in directories)
+            {
+                directoryListBox.Items.Add(directory);
+            }
+            SaveDirectories();
+        }
+
+        private void SaveDirectories()
+        {
+            string buildString = "";
+            foreach (string directory in directories)
+            {
+                buildString += directory + "\n";
+            }
+            File.WriteAllText(("settings.txt"), buildString);
+        }
+
+        private void AddDirectoryButton_Clicked(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    string filePath = folderBrowserDialog.SelectedPath;
+                    if (directories.Contains(filePath) == false)
+                    {
+                        directories.Add(filePath);
+                        ReloadListItems();
+                    }
+                }
+            }
+        }
+
+        private void RemoveDirectoryButton_Clicked(object sender, EventArgs e)
+        {
+            directories.Remove(directoryListBox.SelectedItem.ToString());
+            ReloadListItems();
+        }
+
+        private async void EnabledCheckbox_Changed(object sender, EventArgs e)
+        {
+            TwitchLib.Api.Helix.Models.ChannelPoints.UpdateCustomReward.UpdateCustomRewardRequest updateCustomRewardRequest = new TwitchLib.Api.Helix.Models.ChannelPoints.UpdateCustomReward.UpdateCustomRewardRequest
+            {
+                IsEnabled = enableRewardCheckbox.Checked
+            };
+            await api.Helix.ChannelPoints.UpdateCustomReward(Properties.Settings.Default.ChannelID, Properties.Settings.Default.RewardID, updateCustomRewardRequest);
+        }
+
+        private async void UpdateButton_Clicked(object sender, EventArgs e)
+        {
+            TwitchLib.Api.Helix.Models.ChannelPoints.UpdateCustomReward.UpdateCustomRewardRequest updateCustomRewardRequest = new TwitchLib.Api.Helix.Models.ChannelPoints.UpdateCustomReward.UpdateCustomRewardRequest
+            {
+                Title = titleBox.Text,
+                Cost = Convert.ToInt32(costBox.Text)
+            };
+            Properties.Settings.Default.Title = titleBox.Text;
+            Properties.Settings.Default.Cost = costBox.Text;
+            Properties.Settings.Default.Save();
+            await api.Helix.ChannelPoints.UpdateCustomReward(Properties.Settings.Default.ChannelID, Properties.Settings.Default.RewardID, updateCustomRewardRequest);
+        }
+        private async void ExitButton_Clicked(object sender, EventArgs e)
+        {
+            await api.Helix.ChannelPoints.DeleteCustomReward(Properties.Settings.Default.ChannelID, Properties.Settings.Default.RewardID);
+            Application.Exit();
+        }
+
+        private async void CreateReward()
+        {
+            TwitchLib.Api.Helix.Models.ChannelPoints.CreateCustomReward.CreateCustomRewardsRequest createCustomRewardsRequest = new TwitchLib.Api.Helix.Models.ChannelPoints.CreateCustomReward.CreateCustomRewardsRequest
+            {
+                Title = Properties.Settings.Default.Title,
+                Cost = Convert.ToInt32(Properties.Settings.Default.Cost),
+                IsEnabled = false,
+                ShouldRedemptionsSkipRequestQueue = true
+            };
+            TwitchLib.Api.Helix.Models.ChannelPoints.CreateCustomReward.CreateCustomRewardsResponse createCustomRewardsResponse = await api.Helix.ChannelPoints.CreateCustomRewards(Properties.Settings.Default.ChannelID, createCustomRewardsRequest);
+            Properties.Settings.Default.RewardID = createCustomRewardsResponse.Data[0].Id;
+            enableRewardCheckbox.Checked = createCustomRewardsResponse.Data[0].IsEnabled;
+            costBox.Text = createCustomRewardsResponse.Data[0].Cost.ToString();
+            titleBox.Text = createCustomRewardsResponse.Data[0].Title;
+            Properties.Settings.Default.Save();
+        }
+
+    }
+}
